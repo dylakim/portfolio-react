19b90697ee3a3152f84975ede4f26b337f0cd3d9
Kim Dyla <setilikim@gmail.com>
2026-01-19 17:25:04 -0500

    reorder education and add contentful cert

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,6 +2,13 @@ import type { EducationProps } from 'components/education';
 
 export const education: EducationProps[] = [
   {
+    title: 'Bachelor of Arts',
+    completionYear: '2013',
+    location: 'Michigan State University',
+    description:
+      'Media Arts and Technology degree concentrating on Front-End Web Development, User Experience, and Human Computer Interaction. Specialization in Digital Humanities.',
+  },
+  {
     title: 'Sitecore Personalization Certification',
     completionYear: '2024',
   },
@@ -10,10 +17,7 @@ export const education: EducationProps[] = [
     completionYear: '2024',
   },
   {
-    title: 'Bachelor of Arts',
-    completionYear: '2013',
-    location: 'Michigan State University',
-    description:
-      'Media Arts and Technology degree concentrating on Front-End Web Development, User Experience, and Human Computer Interaction. Specialization in Digital Humanities.',
+    title: 'Contentful Certified Professional',
+    completionYear: '2025',
   },
 ];
